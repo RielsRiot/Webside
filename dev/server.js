35d46367b7b3cd6b72b4d17035dc3a8875276bf9
Flutterish <CharmindYT@gmail.com>
2025-05-03 16:00:35 +0200

    Update server.js

--- dev/server.js
+++ dev/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 var compression = require('compression');
 const path = require('path');
+const process = require('process');
+const child = require('child_process');
+
+let type = 'build';
+if ( process.argv[2] == '-dev' )
+    type = 'dev';
+
+console.log( 'mode = ' + type );
+
+if ( type == 'build' ) {
+    child.execFileSync( 'node', ['./build.js'], { stdio: 'inherit' } );
+}
 
 const app = express();
 
 app.use(compression());
-app.use( express.static( path.join(__dirname, "../Build") ) );
+app.use( express.static( path.join(__dirname, type == 'build' ? '../Build' : "../Public") ) );
 
 app.listen( 8080, () => {
     console.log( 'running on port 8080!' );
